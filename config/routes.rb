6112329c65861f6f976c14b9e9b192cfd6e3d4dd
munroe1786 <era3munroe@gmail.com>
2020-11-07 14:14:57 -0600

    add comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  
   # I want my api to be musicfestivaltracker.com/api/vi/resource
   namespace :api do
     namespace :v1 do
+      resources :comments
       resources :locations
       resources :users
     end
